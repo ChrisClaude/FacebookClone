7d9d049cf3e1792af2fe29f2ec6246f58393f78e
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-08-19 19:40:09 +0200

    Create HostingExtensions.cs

---
+++ src/FacebookClone.API/HostingExtensions.cs
@@ -0,0 +1,28 @@
+namespace FacebookClone.API;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigureRequestPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        return app;
+    }
+}
